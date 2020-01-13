40fe9a0cccc137aac930a232b637773d37b53e5c
kjd95110 <std95110@naver.com>
2020-01-13 17:57:51 +0900

    2020-01-13:pm:06:00

--- OOP/object_bind.js
+++ OOP/object_bind.js
@@ -11,3 +11,10 @@ console.log(sum.call(lee,': '));
 
 var kimSum=sum.bind(kim,'->');
 console.log('kimSum()',kimSum());
+ /* 
+ call: 실행할때 함수의 콘텍스트 즉 this의 값을 바꾼다.
+
+ bind:어떤함수의 내부적으로 this의값을  영구적으로 바꾸는 새로운 함수를 만든다.
+
+
+ */
